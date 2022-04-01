82f6d4a77849fbbdf3c82d71b297a478d8b9e4c8
gabriely <biestrelinha2004@gmail.com>
2022-04-01 09:16:49 -0300

    1.0.1

--- calculadora.js
+++ calculadora.js
@@ -1,2 +1,11 @@
-var somar =  require("./somar");
+var somar =  require("./modulos/somar");
 console.log(somar(20,67));
+
+var subtrair =  require("./modulos/subtrair");
+console.log(subtrair(20,67));
+
+var multiplicar =  require("./modulos/multiplicar");
+console.log(multiplicar(20,67));
+
+var dividir =  require("./modulos/dividir");
+console.log(dividir(20,67));
